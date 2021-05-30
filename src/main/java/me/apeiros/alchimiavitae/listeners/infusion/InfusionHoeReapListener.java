0eaa12bcc80a657ed4cc0e1ce4946708ecf91ba7
HexaGone68 <61390942+HexaGone68@users.noreply.github.com>
2021-05-29 18:45:41 -0700

    Finish the auto-replant infusion

--- src/main/java/me/apeiros/alchimiavitae/listeners/infusion/InfusionHoeReapListener.java
+++ src/main/java/me/apeiros/alchimiavitae/listeners/infusion/InfusionHoeReapListener.java
@@ -2,6 +2,7 @@ package me.apeiros.alchimiavitae.listeners.infusion;
 
 import me.apeiros.alchimiavitae.AlchimiaVitae;
 import org.bukkit.NamespacedKey;
+import org.bukkit.Particle;
 import org.bukkit.Tag;
 import org.bukkit.entity.Player;
 import org.bukkit.event.EventHandler;
@@ -11,16 +12,20 @@ import org.bukkit.persistence.PersistentDataType;
 
 public class InfusionHoeReapListener implements Listener {
 
+    private final NamespacedKey infusionAutoReplant = new NamespacedKey(AlchimiaVitae.i(), "infusion_autoreplant");
+
     @EventHandler(ignoreCancelled = true)
     public void onHarvestCrop(BlockBreakEvent e) {
         if (Tag.CROPS.isTagged(e.getBlock().getType())) {
             Player p = e.getPlayer();
 
-            NamespacedKey nspk = new NamespacedKey(AlchimiaVitae.i(), "infused");
-
+            // Check if the tool has the Auto-Replant infusion
             if (p.getInventory().getItemInMainHand().getItemMeta().
-                    getPersistentDataContainer().has(nspk, PersistentDataType.BYTE)) {
-                // TODO: Add a function
+                    getPersistentDataContainer().has(infusionAutoReplant, PersistentDataType.BYTE)) {
+                // Spawn block
+                e.getBlock().getWorld().spawnFallingBlock(e.getBlock().getLocation(), e.getBlock().getType().createBlockData());
+                // Spawn particles
+                e.getBlock().getWorld().spawnParticle(Particle.VILLAGER_HAPPY, e.getBlock().getLocation(), 50, 1, 1, 1);
             }
         }
     }
