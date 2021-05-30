efd4b4042166f189ef134d82044c4f976a0352ca
HexaGone68 <61390942+HexaGone68@users.noreply.github.com>
2021-05-30 13:27:11 -0700

    Add TODO

--- src/main/java/me/apeiros/alchimiavitae/listeners/infusion/InfusionAxeAttackListener.java
+++ src/main/java/me/apeiros/alchimiavitae/listeners/infusion/InfusionAxeAttackListener.java
@@ -3,4 +3,8 @@ package me.apeiros.alchimiavitae.listeners.infusion;
 import org.bukkit.event.Listener;
 
 public class InfusionAxeAttackListener implements Listener {
+
+    // TODO: Destructive Criticals Infusion (Chance for explosion on a crit (no block dmg))
+    // TODO: Phantom Criticals Infusion (Chance to partially bypass shield or armor on a crit)
+
 }
--- src/main/java/me/apeiros/alchimiavitae/listeners/infusion/InfusionPickBreakListener.java
+++ src/main/java/me/apeiros/alchimiavitae/listeners/infusion/InfusionPickBreakListener.java
@@ -3,4 +3,7 @@ package me.apeiros.alchimiavitae.listeners.infusion;
 import org.bukkit.event.Listener;
 
 public class InfusionPickBreakListener implements Listener {
+
+    // TODO: Tunneler Infusion (Mines 8 blocks forward)
+
 }
