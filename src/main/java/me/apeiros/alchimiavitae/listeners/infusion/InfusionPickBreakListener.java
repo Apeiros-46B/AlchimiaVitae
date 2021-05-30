3e82d8a76579ef5c81b1083b601faefcbf2a930c
HexaGone68 <61390942+HexaGone68@users.noreply.github.com>
2021-05-30 16:33:26 -0700

    a

--- src/main/java/me/apeiros/alchimiavitae/listeners/infusion/InfusionPickBreakListener.java
+++
@@ -1,9 +0,0 @@
-package me.apeiros.alchimiavitae.listeners.infusion;
-
-import org.bukkit.event.Listener;
-
-public class InfusionPickBreakListener implements Listener {
-
-    // TODO: Tunneler Infusion (Mines 8 blocks forward)
-
-}
