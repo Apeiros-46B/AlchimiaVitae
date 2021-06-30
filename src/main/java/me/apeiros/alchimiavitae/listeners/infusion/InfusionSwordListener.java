7f55498abfe601062a07b44e1b7b566bc00ed5a5
Apeiros-46B <61390942+Apeiros-46B@users.noreply.github.com>
2021-06-30 15:51:42 -0700

    [CI Skip] (untested) add key detection to sword infusion listener

--- src/main/java/me/apeiros/alchimiavitae/listeners/infusion/InfusionSwordListener.java
+++ src/main/java/me/apeiros/alchimiavitae/listeners/infusion/InfusionSwordListener.java
@@ -1,18 +1,22 @@
 package me.apeiros.alchimiavitae.listeners.infusion;
 
 import me.apeiros.alchimiavitae.AlchimiaVitae;
+import org.bukkit.EntityEffect;
 import org.bukkit.Material;
+import org.bukkit.NamespacedKey;
 import org.bukkit.entity.HumanEntity;
 import org.bukkit.entity.Player;
 import org.bukkit.event.EventHandler;
 import org.bukkit.event.Listener;
 import org.bukkit.event.entity.EntityDamageByEntityEvent;
+import org.bukkit.persistence.PersistentDataType;
 
 import java.util.concurrent.ThreadLocalRandom;
 
 public class InfusionSwordListener implements Listener {
 
     // Key
+    private final NamespacedKey infusionShieldDisruptor = new NamespacedKey(AlchimiaVitae.i(), "infusion_shielddisruptor");
 
     // Constructor
     public InfusionSwordListener(AlchimiaVitae p) {
@@ -23,17 +27,25 @@ public class InfusionSwordListener implements Listener {
     @EventHandler(ignoreCancelled = true)
     public void onHit(EntityDamageByEntityEvent e) {
         if (e.getDamager() instanceof Player && e.getEntity() instanceof HumanEntity) {
-            // Generate a random number
-            ThreadLocalRandom r = ThreadLocalRandom.current();
-            int rNum = r.nextInt(4);
+            // Null check
+            if (((Player) e.getDamager()).getInventory().getItemInMainHand().getItemMeta() != null) {
+                if (((Player) e.getDamager()).getInventory().getItemInMainHand().getItemMeta()
+                        .getPersistentDataContainer().has(infusionShieldDisruptor, PersistentDataType.BYTE)) {
+                    // Generate a random number
+                    ThreadLocalRandom r = ThreadLocalRandom.current();
+                    int rNum = r.nextInt(4);
 
-            // Conditions for disabling shield
-            if (((HumanEntity) e.getEntity()).isBlocking() && ((Player) e.getDamager()).isSprinting() && rNum <= 2) {
-                // Disable shield
-                ((HumanEntity) e.getEntity()).setCooldown(Material.SHIELD, 100);
-            } else if (((HumanEntity) e.getEntity()).isBlocking() && rNum == 0) {
-                // Disable shield
-                ((HumanEntity) e.getEntity()).setCooldown(Material.SHIELD, 100);
+                    // Conditions for disabling shield
+                    if (((HumanEntity) e.getEntity()).isBlocking() && ((Player) e.getDamager()).isSprinting() && rNum <= 2) {
+                        // Disable shield
+                        ((HumanEntity) e.getEntity()).setCooldown(Material.SHIELD, 100);
+                        e.getEntity().playEffect(EntityEffect.SHIELD_BREAK);
+                    } else if (((HumanEntity) e.getEntity()).isBlocking() && rNum == 0) {
+                        // Disable shield
+                        ((HumanEntity) e.getEntity()).setCooldown(Material.SHIELD, 100);
+                        e.getEntity().playEffect(EntityEffect.SHIELD_BREAK);
+                    }
+                }
             }
         }
     }
